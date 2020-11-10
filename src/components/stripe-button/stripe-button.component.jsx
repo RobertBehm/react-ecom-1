e75cbd7863dca595a35a8455f0f8fb802bc59978
RobertBehm <59755088+RobertBehm@users.noreply.github.com>
2020-11-09 23:45:03 -0500

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_WEV7eC3FbMoXKHyz4KZTH8QR'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAdrewss
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='PayNow'
+            token={onToken}
+            stripeKey={publishableKey}
+            />
+    );
+};
+
+export default StripeCheckoutButton;
